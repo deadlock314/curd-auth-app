83328bb1f72a3db87f147cea3e5dd81097aca31e
chandan soni <chandansoni2270@gmail.com>
2022-01-07 15:56:35 +0530

    added cookieCredentialsMiddleware , nocacheMiddleware  and fixed cors midd

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,35 @@
 
-
 const express = require('express');
 const mongoose = require('mongoose');
-const server = express();
-const cors=require('cors')();
+const cors=require('cors');
 const cookieParser=require('cookie-parser');
 require('dotenv').config();
+const cookieCredentialsMiddleware=require('./middleware/cookieCredentialsMid')
+
+const server = express();
+
+
+server.use(cookieParser());
 
 mongoose.Promise = global.Promise;
+
 // two middlewars
 // to ignore cross origin request error 
-server.use(cors);
+server.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials: true
+    }
+));
+
+
+server.use(cookieCredentialsMiddleware);
 
 //to sent and recive data in json format
 server.use(express.json());
 
 //to work with cookie easily
-server.use(cookieParser());
+
 
 mongoose.connect(process.env.DB_URI,{
 });
@@ -30,10 +43,14 @@ mongoose.connection.once('open',(err)=>{
     else
     console.log('db connected');
 });
+
+
+
+
 const routes=require('./routes');
 const SignUpRoute=require('./auth/authRoute/signUpRoute');
 const logInRoute=require('./auth/authRoute/logInRoute');
-const logOutRoute=require('./auth/authRoute/logOutRoute')
+const logOutRoute=require('./auth/authRoute/logOutRoute');
 server.use('/',SignUpRoute);
 server.use('/',logInRoute);
 server.use('/',logOutRoute);
